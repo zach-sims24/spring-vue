d4d4a958568a879e0631f7431b5c3f9b25e58b48
zach.sims <zach.sims@funeraldirectorslife.com>
2023-01-02 11:32:56 -0600

    master
    Initial commit.

---
+++ src/test/java/com/example/springvue/SpringVueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springvue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
